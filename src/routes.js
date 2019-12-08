9c775adfc151894d67f4c09c93764b735a1fd98e
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-12-08 09:51:06 -0300

    criando tabela students

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
+routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 export default routes;
